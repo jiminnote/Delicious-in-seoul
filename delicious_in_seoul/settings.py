1f74f6f4130ab684986089c3783a65c5347a8ead
jimin <trace_of_jimin@naver.com>
2022-07-17 22:31:09 +0900

    CREATE : 프로젝트 초기셋팅
    UPDATE : 프로젝트 초기셋팅

--- delicious_in_seoul/settings.py
+++ delicious_in_seoul/settings.py
@@ -12,27 +12,46 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from my_settings import (
+    SECRET_KEY,
+    DATABASES,
+    ALGORITHM,
+    KAKAOAPI_KEY,
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_KEY,
+    IMAGE_URL,
+    AWS_REGION,
+    AWS_STORAGE_BUCKET_NAME,
+    AWS_LOCATION
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = SECRET_KEY
+
+ALGORITHM = ALGORITHM
+
+KAKAOAPI_KEY = KAKAOAPI_KEY
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lji*tta60ti%b7af^sg%!_!zb%vrl7%)2w&*1k*m$fzgt^v29w'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
+    #'django.contrib.admin',
+    # 'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -43,8 +62,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -73,12 +92,7 @@ WSGI_APPLICATION = 'delicious_in_seoul.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DATABASES
 
 
 # Password validation
@@ -121,3 +135,56 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL  =True 
+CORS_ALLOW_CREDENTIALS = True
+    
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+    
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',    		
+)
+
+APPEND_SLASH = False
+
+AWS_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME
+KAKAOAPI_KEY = KAKAOAPI_KEY
+
+AWS_REGION   = AWS_REGION
+IMAGE_URL    = IMAGE_URL
+AWS_SECRET_KEY = AWS_SECRET_KEY
+AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
+AWS_LOCATION =AWS_LOCATION
+
+LOGGING = {
+    'disable_existing_loggers': False,
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
